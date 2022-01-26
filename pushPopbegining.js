b84be33371086d8c15096e6dc4502b1e34d4b898
mehedi hasan <mehedicsebu@gmail.com>
2022-01-27 01:41:22 +0600

    push and pop from last added

--- pushPopbegining.js
+++ pushPopbegining.js
@@ -4,6 +4,8 @@ var nmbr = [39, 40, 45, 10, 44];
 console.log("Here is the result for adding Element in the begining");
 nmbr.unshift(39, 89);
 
-nmbr.shift(); //pop item  from begining
-
+nmbr.shift();//pop item  from begining
+//nmbr.pop();//item deleted from end
+nmbr.push(88888);//add item in the last
 console.log(nmbr);
+
