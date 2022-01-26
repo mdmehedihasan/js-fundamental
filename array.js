92553c5ac18f20966e42844981311a8ccc3d276e
mehedi hasan <mehedicsebu@gmail.com>
2022-01-27 01:38:33 +0600

    push and pop from begining

--- array.js
+++ array.js
@@ -1,4 +1,5 @@
 var foods = ['burger', 'naga', 'chillox'];
 var feeForMuriParty = [10, 39, 44, 33, 44, 55];
+feeForMuriParty.unshift(20);
 // console.log(foods);
-console.log(feeForMuriParty.length);
+console.log(feeForMuriParty);
---
+++ pushPopbegining.js
@@ -0,0 +1,9 @@
+var nmbr = [39, 40, 45, 10, 44];
+// adding number in the begingin
+
+console.log("Here is the result for adding Element in the begining");
+nmbr.unshift(39, 89);
+
+nmbr.shift(); //pop item  from begining
+
+console.log(nmbr);
